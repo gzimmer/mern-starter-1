108f5524b0225aaecba5d4b6a8bef57482d332c3
Zimmer, Greg <gzimmer@cardinalsolutions.com>
2017-08-22 10:02:47 -0400

    Updated deployment steps

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -39,7 +39,7 @@ module.exports = {
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?localIdentName=[hash:base64]&modules&importLoaders=1!postcss-loader'),
+        loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?localIdentName=[hash:base64]&modules&importLoaders=1!postcss-loader'}),
       }, {
         test: /\.css$/,
         include: /node_modules/,
